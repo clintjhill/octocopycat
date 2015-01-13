02bc0b85bee1a2f122c04855d3ec078cdce8f1dd
Clint Hill <clint.hill@gmail.com>
2015-01-13 08:08:51 -0700

    More things. Still learning.

--- src/main.rs
+++ src/main.rs
@@ -4,58 +4,42 @@ extern crate serialize;
 use serialize::{json};
 use std::str::FromStr;
 use std::io::process::{Command, ProcessOutput};
-use std::comm;
 use std::thread::Thread;
+
 use hyper::client::Client;
-use hyper::header::{Headers, Authorization, Accept, UserAgent};
-use hyper::mime::{Mime};
+use hyper::client::Response;
+use hyper::header::Headers;
+use hyper::header::shared::qitem;
+use hyper::header::common::{Authorization, Accept, UserAgent};
+use hyper::mime::Mime;
+use hyper::mime::TopLevel::{Application};
 
-
-fn clone(url: &str, tx: Sender<&str>) {
-	let workspace = Path::new("{env.workspace");
-	match Command::new("git").cwd(&workspace).arg("clone").arg(url).output() {
-		Err(msg) => panic!("Failed to run {}", msg.desc),
-		Ok(ProcessOutput { error: err, output: out, status: exit }) => {
-      if exit.success() {
-      		let s = String::from_utf8_lossy(out.as_slice());
-      		let msg = format!("Cloned {} successfully!\n{}", url, s);
-          tx.send(msg.as_slice());
-      } else {
-          let s = String::from_utf8_lossy(err.as_slice());
-          let msg = format!("Failed to clone: {}\n{}\n\n", url, s);
-          tx.send(msg.as_slice());
-      }
-  	},
-	};
-}
+//mod git;
 
 fn main() {
 
 	let url = "{env.github.api.url}";
-	let token = "token {env.github.api.token}".to_string();
-	let agent = "octocopycat".to_string();
-	let mime: Mime = FromStr::from_str("application/vnd.github.v3+json").unwrap();
+	let token = String::from_str("token {env.github.api.token}");
+	let agent = String::from_str("octocopycat");
+	//let mime: Mime = FromStr::from_str("application/vnd.github.v3+json").unwrap();
 	let mut headers = Headers::new();
 	let mut client = Client::new();
 
-	headers.set(Accept(vec![mime]));
+	headers.set(Accept(vec![
+		qitem(Mime(Application, "vnd.github.v3+json", vec![]))
+		]));
+
 	headers.set(Authorization(token));
 	headers.set(UserAgent(agent));
 
-	let mut response = match client.get(url).headers(headers).send() {
-		Ok(response) => response,
-		Err(msg) => panic!("There was an error getting url: {}", msg)
+	let mut response: Response = match client.get(url).headers(headers).send() {
+		Err(msg) => panic!("Failed to connect: {}", msg),
+		Ok(r) => r
 	};
 
-	let content = match response.read_to_string() {
-		Ok(content) => content,
-		Err(msg) => panic!("There was a problem reading response {}", msg)
-	};
-
-	let json_body = match json::from_str(content.as_slice()) {
-		Ok(json_body) => json_body,
-		Err(msg) => panic!("There was problem parsing json {}", msg)
-	};		
+	/*
+	let content = try!(response.read_to_string());
+	let json_body = try!(json::from_str(content.as_slice()));
 
 	let repositories = json_body.as_array().unwrap();
 	let (tx, rx): (Sender<&str>, Receiver<&str>) = comm::channel();
@@ -64,11 +48,12 @@ fn main() {
 		let url = location.find("ssh_url").unwrap().clone();
 		let thread_tx = tx.clone();
 		Thread::spawn(move || {
-			clone(url.as_string().unwrap(), thread_tx)
+			git::clone(url.as_string().unwrap(), thread_tx)
 		}).detach();
 	};
 
 	for message in rx.iter() {
 		println!("Message: {}", message)
 	};
+	*/
 }
