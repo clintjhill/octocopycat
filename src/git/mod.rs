c3b0fe19b6c1b68c9882354686a9dffd0ead4346
Clint Hill <clint.hill@gmail.com>
2015-01-24 17:18:06 -0800

    Adding some threads.

--- src/git/mod.rs
+++ src/git/mod.rs
@@ -11,23 +11,27 @@ use std::io::process::{Command, ProcessOutput};
 use std::fmt;
 use self::serialize::json;
 use env::Environment;
+use std::thread::Thread;
 
 pub fn clone(url: String, workspace: String) {
 
 	let workspace = Path::new(workspace);
 
-	match Command::new("git").cwd(&workspace).arg("clone").arg(url).output() {
-		Err(msg) => panic!("Failed to run {}", msg.desc),
-		Ok(ProcessOutput { error: err, output: out, status: exit }) => {
-      if exit.success() {
-      		let s = String::from_utf8_lossy(out.as_slice());
-      		println!("{}\n", s);
-      } else {
-          let s = String::from_utf8_lossy(err.as_slice());
-          println!("Failed to clone: {}\n\n", s);
-      }
-  	},
-	};
+  Thread::spawn(move || {
+    match Command::new("git").cwd(&workspace).arg("clone").arg(url).output() {
+      Err(msg) => panic!("Failed to run {}", msg.desc),
+      Ok(ProcessOutput { error: err, output: out, status: exit }) => {
+        if exit.success() {
+            let s = String::from_utf8_lossy(out.as_slice());
+            println!("{}\n", s);
+        } else {
+            let s = String::from_utf8_lossy(err.as_slice());
+            println!("Failed to clone: {}\n\n", s);
+        }
+      },
+    };
+  });
+	
 }
 
 pub fn repos(env_config: Environment) -> Vec<json::Json> {
