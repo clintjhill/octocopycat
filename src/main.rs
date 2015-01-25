7de2967a4873f5c954c471a74287767a0c381c4a
Clint Hill <clint.hill@gmail.com>
2015-01-25 08:00:57 -0800

    formatting.

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,14 @@ mod git;
 mod env;
 
 fn main() {
-	let env_config: env::Environment = env::get("env.toml");	
-	
+	let env_config: env::Environment = env::get("env.toml");
+
 	let repositories: Vec<Json> = git::repos(env_config.clone());
 	let mut messages: Vec<String> = Vec::new();
 	let (tx, rx): (Sender<String>, Receiver<String>) = mpsc::channel();
 
 	for location in repositories.iter() {
-		let url = String::from_str(location.find("ssh_url").unwrap().as_string().unwrap());	
+		let url = String::from_str(location.find("ssh_url").unwrap().as_string().unwrap());
 		let thread_tx = tx.clone();
 		git::clone(url, env_config.workspace.clone(), thread_tx);
 	};
